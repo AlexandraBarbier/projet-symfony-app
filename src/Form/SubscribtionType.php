6127a9ffbaf2d99055950b0ec8cf9da0c12eeb23
AlexandraBarbier <graphikstaff@gmail.com>
2022-02-09 12:25:54 +0100

    Configuration du formulaire d'inscription

--- src/Form/SubscribtionType.php
+++ src/Form/SubscribtionType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,21 @@ class SubscribtionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email :'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                'label' => 'Votre mot de passe :'
+                ],
+                'second_options' => [
+                    'label' => 'Répeter votre mot de passe :'
+                    ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire'
+            ])
         ;
     }
 
@@ -22,6 +38,7 @@ class SubscribtionType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'method' => 'POST'
         ]);
     }
 }
